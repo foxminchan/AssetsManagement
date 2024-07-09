0c4d95121f459319cf08505b63018c3bcfd90dc1
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-07-10 01:01:49 +0700

    (#3586) Admin is able to view asset list and detailed asset information

---
+++ tests/ASM.IntegrationTest/Features/Assets/GetAssetTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using ASM.Application.Domain.AssetAggregate;
+using ASM.Application.Domain.Shared;
+using ASM.Application.Features.Assets;
+using ASM.IntegrationTest.Extensions;
+using ASM.IntegrationTest.Fakers;
+using ASM.IntegrationTest.Fixtures;
+
+namespace ASM.IntegrationTest.Features.Assets;
+
+public sealed class GetAssetTests(ApplicationFactory<Program> factory)
+    : IClassFixture<ApplicationFactory<Program>>, IAsyncLifetime
+{
+    private readonly ApplicationFactory<Program> _factory = factory.WithDbContainer();
+
+    public async Task InitializeAsync() => await _factory.StartContainersAsync();
+
+    public async Task DisposeAsync() => await _factory.StopContainersAsync();
+
+    [Fact]
+    public async Task GivenAssetId_ShouldReturnNotFound_IfAssetsNotExists()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var id = Guid.NewGuid();
+
+        // Act
+        var response = await client.GetAsync($"/api/assets/{id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GivenAssetId_ShouldReturnSuccess()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var asset = new AssetFaker().Generate(1);
+        asset[0].Location = Location.HoChiMinh;
+        var category = new Category { Id = Guid.NewGuid(), Name = "Category 1", Prefix = "C1" };
+        asset[0].CategoryId = category.Id;
+        var id = asset[0].Id;
+
+        // Act
+        await _factory.EnsureCreatedAndPopulateDataAsync([category]);
+        await _factory.EnsureCreatedAndPopulateDataAsync(asset);
+        var response = await client.GetAsync($"/api/assets/{id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var assetResponse = await response.Content.ReadFromJsonAsync<AssetDto>();
+        assetResponse.Should().NotBeNull();
+        assetResponse.Id.Should().Be(id);
+    }
+}
---
+++ tests/ASM.IntegrationTest/Features/Categories/ListCategoriesTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using ASM.Application.Domain.AssetAggregate;
+using ASM.IntegrationTest.Extensions;
+using ASM.IntegrationTest.Fixtures;
+
+namespace ASM.IntegrationTest.Features.Categories;
+
+public sealed class ListCategoriesTests(ApplicationFactory<Program> factory)
+    : IClassFixture<ApplicationFactory<Program>>, IAsyncLifetime
+{
+    private readonly ApplicationFactory<Program> _factory = factory.WithDbContainer();
+
+    public async Task InitializeAsync() => await _factory.StartContainersAsync();
+
+    public async Task DisposeAsync() => await _factory.StopContainersAsync();
+
+    [Fact]
+    public async Task GivenValidRequest_ShouldReturnSuccess()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        var categories = new List<Category>
+        {
+            new("Category 1", "C1"), new("Category 2", "C2"), new("Category 3", "C3"), new("Category 4", "C4"),
+        };
+
+        // Act
+        await _factory.EnsureCreatedAndPopulateDataAsync(categories);
+        var response = await client.GetAsync("/api/categories");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
